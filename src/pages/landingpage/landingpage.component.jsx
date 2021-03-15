88790dc3a25bb75c53daa2addd293fe9b7ac7399
Princessbolade <mobolade.akinlua@outlook.com>
2021-03-15 22:48:07 +0100

    NavBar Done

--- src/pages/landingpage/landingpage.component.jsx
+++ src/pages/landingpage/landingpage.component.jsx
@@ -1,24 +1,13 @@
 import React from "react";
 
-import "./landingpage.css";
-
 const LandingPage = () => (
-  <div className="landingpage">
-    <div className="body">
-      <h1 className="landingheader">
-        <strong>
-          A Digital <br /> Savings Website.
-        </strong>
-      </h1>
-      <p className="landingtext">
-        DIGIAJO is a rotational savings club aimed at a group of individuals to
-        save <br /> together in a form of peer-to-peer savings.Group members
-        will contribute funds
-        <br /> into a pool regularly, and take turns withdrawing funds from the
-        pool.
-      </p>
-      <button className="getstartedbutton">GET STARTED</button>
-    </div>
+  <div
+    className="w-full md:w-9/12 xl:w-8/12"
+    style={{
+      backgroundColor: "white",
+    }}
+  >
+    HELLO WORLD
   </div>
 );
 
