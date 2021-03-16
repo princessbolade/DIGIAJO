3030f8d544bb74c723cff2ee086aae3855b7b284
Oluwatosin Bakare <oluwatosin.bakare@yahoo.com>
2021-03-16 09:42:33 +0100

    Added about us page to the route

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 
-import LandingPage from "./pages/LandingPage/landingpage.component";
+import LandingPage from "./pages/landingpage/landingpage.component";
 import Header from "./components/header/header.component";
+import AboutUs from './pages/aboutus/aboutus.component';
 
 import "./App.css";
 
@@ -10,8 +11,10 @@ function App() {
   return (
     <div className="App">
       <Header />
+
       <Switch>
         <Route exact path="/" component={LandingPage} />
+        <Route exact path="/aboutus" component={AboutUs} />
       </Switch>
     </div>
   );
