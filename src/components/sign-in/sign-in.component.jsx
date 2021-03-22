a8aaf924d878dcb75b367a3f99c05514c074e44e
Princessbolade <mobolade.akinlua@outlook.com>
2021-03-22 09:55:12 +0100

    LandingPage and Show Password Toggle

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,73 +1,86 @@
-import React from "react";
+import React, { useState } from "react";
 import SignInImg from "../../assets/signIn.png";
 import { Link } from "react-router-dom";
 
-const SignIn = () => (
-  <div className="lg:flex">
-    <div className="lg:w-1/2 xl:max-w-screen-sm overflow-hidden">
-      <div class="py-12 bg-gray-200 lg:bg-white flex justify-center lg:justify-start lg:px-12">
-        <div class="cursor-pointer flex items-center"> </div>
-        <div class="text-2xl uppercase text-blue-900 tracking-wide ml-2 font-semibold"></div>
-      </div>
-      <div className="mt-10 ml-20 px-12 sm:px-24 md:px-48 lg:px-12 lg:mt-16 xl:px-24 xl:max-w-2xl">
-        <h2 className="text-center text-4xl text-blue-900 font-semibold lg:text-left xl:text-5xl xl:text-bold">
-          SIGN IN
-        </h2>
-        <div className="mt-12">
-          <form>
-            <div className="text-sm font-bold text-gray-700 tracking-wide">
-              Email Address
-            </div>
-            <input
-              className="w-full text-lg py-2 border-b border-gray-300 focus:outline-none focus:border-blue-900"
-              type=""
-              placeholder="person@gmail.com"
-            />
-            <div className="mt-8">
-              <div className="flex justify-between items-center">
-                <div className="text-sm font-bold text-gray-700 tracking-wide">
-                  Password
-                </div>
-                <div>
-                  <a className="text-xs font-semibold text-blue-900">
-                    Forgot Password?
-                  </a>
-                </div>
+const SignIn = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  return (
+    <div className="lg:flex">
+      <div className="lg:w-1/2 xl:max-w-screen-sm overflow-hidden">
+        <div class="py-12 bg-gray-200 lg:bg-white flex justify-center lg:justify-start lg:px-12">
+          <div class="cursor-pointer flex items-center"> </div>
+          <div class="text-2xl uppercase text-blue-900 tracking-wide ml-2 font-semibold"></div>
+        </div>
+        <div className="mt-10 ml-20 px-12 sm:px-24 md:px-48 lg:px-12 lg:mt-16 xl:px-24 xl:max-w-2xl">
+          <h2 className="text-center text-4xl text-blue-900 font-semibold lg:text-left xl:text-5xl xl:text-bold">
+            SIGN IN
+          </h2>
+          <div className="mt-12">
+            <form>
+              <div className="text-sm font-bold text-gray-700 tracking-wide">
+                Email Address
               </div>
               <input
-                className=" w-full text-lg py-2 border-b border-gray-300 focus:outline-none focus:border-blue-900"
-                type="password"
-                placeholder="Enter your password"
+                className="w-full text-lg py-2 border-b border-gray-300 focus:outline-none focus:border-blue-900"
+                type=""
+                placeholder="person@gmail.com"
               />
+              <div className="mt-8">
+                <div className="flex justify-between items-center">
+                  <div className="text-sm font-bold text-gray-700 tracking-wide">
+                    Password
+                  </div>
+                  <div>
+                    <a className="text-xs font-semibold text-blue-900">
+                      Forgot Password?
+                    </a>
+                  </div>
+                </div>
+                <div className=" relative">
+                  <input
+                    className=" w-full text-lg py-2 border-b border-gray-300 focus:outline-none focus:border-blue-900"
+                    type={`${showPassword ? "text" : "password"}`}
+                    placeholder="Enter your password"
+                  />
+                  <span
+                    onClick={() => setShowPassword((prev) => !prev)}
+                    class="z-10 h-full cursor-pointer leading-snug font-normal text-center text-gray-400 absolute bg-transparent rounded text-base items-center justify-center w-8 right-0 pr-3 py-3"
+                  >
+                    <i class="far fa-eye"></i>
+                  </span>
+                </div>
+              </div>
+              <div className="mt-10">
+                <button className="bg-blue-900 text-gray-100 p-4 w-full rounded-full tracking-wide font-semibold focus:outline-none focus:shadow-outline hover:bg-blue-700">
+                  Sign In
+                </button>
+              </div>
+            </form>
+            <div className="mt-12 text-sm font-display font-semibold text-gray-400 text-center">
+              Don't have an account?
+              <Link to="/signup">
+                {" "}
+                <a className="ml-2 cursor-pointer text-blue-900">
+                  SIGN UP
+                </a>{" "}
+              </Link>
             </div>
-            <div className="mt-10">
-              <button className="bg-blue-900 text-gray-100 p-4 w-full rounded-full tracking-wide font-semibold focus:outline-none focus:shadow-outline hover:bg-blue-700">
-                Sign In
-              </button>
-            </div>
-          </form>
-          <div className="mt-12 text-sm font-display font-semibold text-gray-400 text-center">
-            Don't have an account?
-            <Link to="/signup">
-              {" "}
-              <a className="ml-2 cursor-pointer text-blue-900">SIGN UP</a>{" "}
-            </Link>
           </div>
         </div>
       </div>
+      <div
+        className=" lg:flex-items-center justify-center bg-white flex-1 "
+        style={{
+          backgroundImage: `url(${SignInImg})`,
+          backgroundSize: "contain",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+          marginBottom: "20px",
+          marginTop: "20px",
+        }}
+      ></div>
     </div>
-    <div
-      className=" lg:flex-items-center justify-center bg-white flex-1 "
-      style={{
-        backgroundImage: `url(${SignInImg})`,
-        backgroundSize: "contain",
-        backgroundPosition: "center",
-        backgroundRepeat: "no-repeat",
-        marginBottom: "20px",
-        marginTop: "20px",
-      }}
-    ></div>
-  </div>
-);
+  );
+};
 
 export default SignIn;
